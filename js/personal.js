e0fe43d0700eda0ec92549a70dd3ed34808de245
gbrlQuiroz <orion_fire_qog@hotmail.com>
2015-10-29 13:53:37 -0600

    modificaciones
    diseñe el factory para peticiones de tipo REST

--- js/personal.js
+++ js/personal.js
@@ -1,13 +1,25 @@
 (function () {
     var app = angular.module('personal', []);
+    
+    app.factory('RESTful',['$http', function ($http){
+            var response = {};
+            response.getDatos = function (){
+                return $http.get('/api/datos');
+            };
+            response.postDatos = function (datos){
+                return $http.post('',datos);
+            };
+            return response;
+    }]);
 
     
 
     app.controller('meController', ['$scope', function ($scope) {
-        $scope.informa = 'Hola'; //esto es una variable de alcance goblal
+        $scope.informa = 'Hola'; 
         $scope.salvar = function (datitos){
             $scope.informa = "entro al método salvar";
-        }
+            console.log('datitos' + datitos.nombre);
+        };
         
     }]);
 
