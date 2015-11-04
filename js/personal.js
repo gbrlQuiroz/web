6fd837244efcec51c6fb222c383f34f4842cc6ae
gbrlQuiroz <orion_fire_qog@hotmail.com>
2015-11-04 09:51:55 -0600

    Avance progresivo
    varias adecuaciones y mejoras en el código backend

--- js/personal.js
+++ js/personal.js
@@ -4,10 +4,19 @@
     app.factory('RESTful', ['$http', function ($http) {
             var response = {};
             response.getDatos = function () {
-                return $http.get('api/datos/1'); //Obtiene un solo registro 
+                return $http.get('api/datos');
             };
             response.postDatos = function (datos) {
                 return $http.post('api/datos', datos);
+            };
+            response.getDato = function (id) {
+                var uri = 'api/datos/' + id;
+                return $http.get(uri);
+            };
+            response.putDato = function (id, datos) {
+                var uri = 'api/datos/' + id;
+                return $http.put(uri, datos);
+                //return $http.put(id,datos);
             };
             return response;
         }]);
@@ -16,20 +25,18 @@
 
     app.controller('meController', ['$scope', 'RESTful', function ($scope, RESTful) {
 
-            cargarDatos();
-            function cargarDatos() {
-                RESTful.getDatos()
+            cargarDato(1);
+            function cargarDato(id) {
+                RESTful.getDato(id)
                         .success(function (datosCargados) {
                             //console.log("------- " + datosCargados);
                             $scope.datitos = datosCargados;
                         });
-
-            }
-            ;
+            };
 
             $scope.salvar = function (datitos) {
-                $scope.informa = 'entro al método salvar';
-                //console.log('entro al método salvar');
+                $scope.informa = 'entro al método SALVAR';
+                //console.log('entro al método SALVAR');
                 RESTful.postDatos(datitos)
                         .success(function (data, status) {
                             $scope.informa = 'se almacenó el registro';
@@ -40,6 +47,22 @@
                     //console.log('falló la agregación del registro');
                 });
             };
+
+            $scope.modificar = function (datitos) {
+                $scope.informa = 'entro al mètodo MODIFICAR';
+                console.log('entró al método MODIFICAR');
+                RESTful.putDato(1, datitos) //++++++++cuidado!!!  estoy modificando el registro 1  fijo!!!!!++++++++++
+                        .success(function (data, status) {
+                            $scope.informa = 'se modificó registro';
+                            console.log('se modificó registro');
+                        }).error(function (data, status) {
+                    $scope.informa = 'falló la modificación del registro';
+                    console.log('fallo la modificación del registro');
+                });
+
+            };
+
+
         }]);
 
 })();
