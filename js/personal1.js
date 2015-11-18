eb84f0c49b254d610bf8f0449f631177294cd303
antonielle <marytony0606@hotmail.com>
2015-11-18 16:34:03 -0600

    Nuevos
    Se creo otro archivo .html y .js para aplicar la directiva sugerida
    ng-repeat, (Iteración) asi como el correcto manejo del controlador, se
    agregaron datos a los campos del formulario, para iniciar el trabajo con
    tablas y sus css.

--- js/personal1.js
+++ js/personal1.js
@@ -1,9 +1,14 @@
-var app = angular.module('personal1',[]);
+var app = angular.module('persona1',[]);
 
-app.controller('meController', function($scope) {
-  $scope.datitos = ['nombre','fecNac','estatura','peso','titulo']
-});
-      
+app.controller('myController', function($scope){
+  $scope.datos=[
+         {id:1,nombre:"Antonieta",fecNac:"06/06/1972",estatura:"1.60",peso:"55",titulo:"Lic.Informatica"},
+         {id:2,nombre:"Jose",fecNac:"02/03/1979",estatura:"1.60",peso:"55",titulo:"Ing.Sistemas"},
+         {id:3,nombre:"Miguel",fecNac:"07/07/1977",estatura:"1.75",peso:"68",titulo:"Ing.Industrial"},
+         {id:4,nombre:"Mariana",fecNac:"20/08/1989",estatura:"1.60",peso:"58",titulo:"Lic.Derecho"},
+         {id:5,nombre:"Diego",fecNac:"01/09/1985",estatura:"1.62",peso:"55",titulo:"Ing.Sistemas"}
+    ];
+  });
     
   
 
