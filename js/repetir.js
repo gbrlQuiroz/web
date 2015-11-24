b7cd395738b685163c10483cd61c0a8079f0ee74
antonielle <marytony0606@hotmail.com>
2015-11-24 16:35:56 -0600

    Ajustes al js
    La idea es que jale el json en el archivo nuevo que yo cree! errores de
    syntaxis..

--- js/repetir.js
+++ js/repetir.js
@@ -1,11 +1,10 @@
 (function () {
-    var app = angular.module('persona', []);
+    var app = angular.module('personal', []);
 
     app.factory('RESTful', ['$http', function ($http) {
             var response = {};
             response.getDatos = function () {
                 return $http.get('api/datos');
-                 return $http.get('datos.json');
             };
             response.postDatos = function (datos) {
                 return $http.post('api/datos', datos);
@@ -19,51 +18,26 @@
                 return $http.put(uri, datos);
                 //return $http.put(id,datos);
             };
+            response.getRaro = function () {
+                return $http.get('js/datos.json');
+            };
             return response;
         }]);
 
 
 
-    app.controller('meController', ['$scope', 'RESTful', function ($scope, RESTful) {
+    app.controller('meController', '$scope', 'RESTful', function ($scope, RESTful){
 
-            cargarDato(1);
-            function cargarDato(id) {
-                RESTful.getDato(id)
+            /*+++++++++++++++++++++++
+             * 
+             ++++++++++++++++++++++*/
+            cargarRaro();
+            function cargarRaro() {
+                RESTful.getRaro()
                         .success(function (datosCargados) {
-                            //console.log("------- " + datosCargados);
+                            console.log('------' + datosCargados);
                             $scope.datos = datosCargados;
                         });
             };
-
-            $scope.salvar = function (datitos) {
-                $scope.informa = 'entro al método SALVAR';
-                //console.log('entro al método SALVAR');
-                RESTful.postDatos(datitos)
-                        .success(function (data, status) {
-                            $scope.informa = 'se almacenó el registro';
-                            //console.log('se almacenó el registro');
-
-                        }).error(function (data, status) {
-                    $scope.informa = 'falló la agregacion del registro';
-                    //console.log('falló la agregación del registro');
-                });
-            };
-
-            $scope.modificar = function (datitos) {
-                $scope.informa = 'entro al mètodo MODIFICAR';
-                console.log('entró al método MODIFICAR');
-                RESTful.putDato(1, datitos) //++++++++cuidado!!!  estoy modificando el registro 1  fijo!!!!!++++++++++
-                        .success(function (data, status) {
-                            $scope.informa = 'se modificó registro';
-                            console.log('se modificó registro');
-                        }).error(function (data, status) {
-                    $scope.informa = 'falló la modificación del registro';
-                    console.log('fallo la modificación del registro');
-                });
-
-            };
-
-
-        }]);
-
-})();
+    });
+});
